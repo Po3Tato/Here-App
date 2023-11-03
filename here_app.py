76c03e82aab8c958edfd35efe614dbb331081f68
Jude <31327412+Po3Tato@users.noreply.github.com>
2023-11-03 13:04:53 -0500

    Added ability to display next name
    When a given input is received, the out put displays the next student.

--- here_app.py
+++ here_app.py
@@ -1,12 +1,24 @@
+# import docx module
 from docx import Document
 
-def print_name(table):
-    student_name = table.cell(1, 0) # cell start point
-    print(student_name.text)
+def print_name(table, row=1):
+    # loop start here
+    for row_number in range(1, len(table.rows)):
+        student_name = table.cell(row_number, 0) 
+        print(student_name.text) 
+        # user input
+        user_input = input("Enter 1 for 'here', 0 for 'absent' or press 'Enter' to close: ")
 
-# document location
-doc_path = 'E://NO//CODE//projects//here-app-repo//class_attendance.docx'  
+        if user_input == '1':
+            continue
+        elif user_input == '0':
+            print("Not here.")
+        elif user_input == '':
+            print("Closing...")
+            break
+# path of file. Change later [refer to notes on notion]
+doc_path = 'E://NO//CODE//projects//here-app-repo//class_attendance.docx'
 doc = Document(doc_path)
 table = doc.tables[0]
+
 print_name(table)
-input("Press Enter to close...")
